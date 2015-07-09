afcc3c6f04d0bb4b32ad33b02ea5e584972c6169
erik arfvidson <erik.arfvidson@gmail.com>
2015-07-08 22:17:14 -0400

    freebooter: add map fixes
    debbuging map

--- app/src/main/java/com/softwarewarfare/freebooter/GoogleMaps.java
+++ app/src/main/java/com/softwarewarfare/freebooter/GoogleMaps.java
@@ -5,86 +5,93 @@ package com.softwarewarfare.freebooter;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleMaps extends FragmentActivity {
 
-    /** Local ariables **/
-    GoogleMap googleMap;
-
+public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_maps);
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_google_maps, menu);
-        return true;
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    /**
-     * Initialises the mapview
-     */
-    private void createMapView(){
-        /**
-         * Catch the null pointer exception that
-         * may be thrown when initialising the map
-         */
-        try {
-            if(null == googleMap){
-                googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-                        R.id.mapView)).getMap();
-
-                /**
-                 * If the map is still null after attempted initialisation,
-                 * show an error to the user
-                 */
-                if(null == googleMap) {
-                    Toast.makeText(getApplicationContext(),
-                            "Error creating map", Toast.LENGTH_SHORT).show();
-                }
-            }
-        } catch (NullPointerException exception){
-            Log.e("mapApp", exception.toString());
-        }
-    }
-    /**
-     * Adds a marker to the map
-     */
-    private void addMarker(){
-
-        /** Make sure that the map has been initialised **/
-        if(null != googleMap){
-            googleMap.addMarker(new MarkerOptions()
-                            .position(new LatLng(0, 0))
-                            .title("Marker")
-                            .draggable(true)
-            );
-        }
-    }
 }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//    /**
+//     * Initialises the mapview
+//     */
+//    private void createMapView(){
+//        /**
+//         * Catch the null pointer exception that
+//         * may be thrown when initialising the map
+//         */
+//        try {
+//            if(null == googleMap){
+//                googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+//                        R.id.mapView)).getMap();
+//
+//                /**
+//                 * If the map is still null after attempted initialisation,
+//                 * show an error to the user
+//                 */
+//                if(null == googleMap) {
+//                    Toast.makeText(getApplicationContext(),
+//                            "Error creating map", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        } catch (NullPointerException exception){
+//            Log.e("mapApp", exception.toString());
+//        }
+//    }
+//    /**
+//     * Adds a marker to the map
+//     */
+//    private void addMarker(){
+//
+//        /** Make sure that the map has been initialised **/
+//        if(null != googleMap){
+//            googleMap.addMarker(new MarkerOptions()
+//                            .position(new LatLng(0, 0))
+//                            .title("Marker")
+//                            .draggable(true)
+//            );
+//        }
+//    }
+//}
+//
+//
